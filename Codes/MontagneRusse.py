154fa80ed4cf7cb9bba17c81055f6f7241cd33de
Louis Cherel <louis.cherel@rwth-aachen.de>
2021-01-26 12:07:58 +0100

    Ajout des collisions

--- Codes/MontagneRusse.py
+++ Codes/MontagneRusse.py
@@ -22,17 +22,25 @@ class Train():
         self.tail = [(self.position,100)]
     def update(self):
         self.change_acc()
-        if self.speed+self.acc >= self.minSpeed:
-            self.speed+=self.acc
-        if self.goingUp:
-            self.position += self.speed
-        else:
-            self.position -= self.speed
         
+        self.position += self.speed
+
+        if abs(self.speed+self.acc) >= abs(self.minSpeed) or self.minSpeed == 0:
+            self.speed+=self.acc
         return self.updateTail()
     
     def change_acc(self):
         self.acc = self.master.getAcc(int(self.position))
+    
+    def get_acc(self):
+        return self.acc
+    def get_speed(self):
+        return self.speed
+    
+    def set_speed(self,new_speed):
+        self.speed = new_speed
+    def set_pos(self,new_position):
+        self.position = new_position
     
     def getColor(self, intensity):
         newColor = [0,0,0]            
@@ -41,35 +49,40 @@ class Train():
         return Color(newColor[0], newColor[1], newColor[2])
     def getPositions(self):
         return [v[0] for v in self.tail]
+    def getPosition(self):
+        return self.position
     def getColors(self):
         return [self.getColor(v[1]) for v in self.tail]
     def updateTail(self):
         newTail = []
         position = int(self.position)
-        if position != self.tail[-1][0]:
+        for v in self.tail:
             #Updating previous pixels
-            for v in self.tail:
-                if v[1] > 0:
-                    newTail.append((v[0],max(0,v[1]-self.tailFactor)))
-            if len(newTail) > 0:
-                if self.goingUp == ( self.speed>0 ):    
-                    newTail += [(k,newTail[-1][1]+((100-newTail[-1][1])/(position-newTail[-1][0]))*(k-newTail[-1][0])) for k in range(newTail[-1][0]+1,min(position,max(self.bounds))+1)]
-                else:
-                    newTail += [(k,newTail[-1][1]+((100-newTail[-1][1])/abs(position-newTail[-1][0]))*abs(k-newTail[-1][0])) for k in range(newTail[-1][0]-1,max(position-1,min(self.bounds)-1),-1)]
-            self.tail = newTail
+            if v[1] > 0:
+                newTail.append((v[0],max(0,v[1]-self.tailFactor)))
+        if len(newTail) > 0:
+            if position != newTail[-1][0]:
+                    if ( self.speed>0 ):    
+                        newTail += [(k,newTail[-1][1]+((100-newTail[-1][1])/(position-newTail[-1][0]))*(k-newTail[-1][0])) for k in range(newTail[-1][0]+1,min(position,max(self.bounds))+1)]
+                    else:
+                        newTail += [(k,newTail[-1][1]+((100-newTail[-1][1])/abs(position-newTail[-1][0]))*abs(k-newTail[-1][0])) for k in range(newTail[-1][0]-1,max(position-1,min(self.bounds)-1),-1)]
+            else:
+                newTail[-1] = (position,100)
+        self.tail = newTail
         return len(self.tail) == 0
                 
     
     
     
 class Rails():
-    def __init__(self, strip, bounds, boundsFalling, boundsRising, color = (255,255,255), initialSpeed = 0,minSpeed = 1, g = 0.1, goingUp = False, tailFactor = 30, newTrainDelay = 50, randomColor = True, randomNewTrainDelay = 30,maxTrainNb = 3):
+    def __init__(self, strip, bounds, boundsFalling, boundsRising, color = (255,255,255), initialSpeed = 0, randomSpeed = 3.5,minSpeed = 1, g = 0.1, goingUp = False, tailFactor = 30, newTrainDelay = 30, randomColor = True, randomNewTrainDelay = 1,maxTrainNb = 6):
         self.strip = strip
         self.bounds = bounds
         self.boundsFalling = boundsFalling
         self.boundsRising = boundsRising
         self.color = color
         self.speed = initialSpeed
+        self.randomSpeed = randomSpeed
         self.minSpeed = minSpeed
         self.g = g
         self.goingUp = goingUp
@@ -89,33 +102,81 @@ class Rails():
                 self.makeNewTrain()
         else:
             self.count += 1
-        #update des trains
-        for f in self.trains:
-            if f.update():
-                self.trains.remove(f)
-                
-            else:
-                pos = f.getPositions()
-                col = f.getColors()
-                for k,v in enumerate(pos):
-                    self.strip.setPixelColor(v,col[k])
+
+        collision = [False for t in self.trains]        
+        for k,i in enumerate(self.trains):
+            i_pos = i.getPosition()
+            i_speed = i.get_speed()
+            
+            for l,j in enumerate(self.trains[k+1:]):
+                j_pos = j.getPosition()
+                j_speed = j.get_speed()
+                if (i_pos-j_pos)*(i_pos+i_speed-j_pos-j_speed)<=0:
+                    collision[k] = True
+                    collision[k+l+1]= True
+                    new_pos = int((i_pos+i_speed+j_pos+j_speed)/2)
+                    if i_pos < j_pos:
+                        i.set_pos(new_pos)
+                        j.set_pos(new_pos+1)
+                    else :
+                        i.set_pos(new_pos+1)
+                        j.set_pos(new_pos)
+                    
+                    i.updateTail()
+                    i.set_speed(j_speed)
+                    
+                    j.updateTail()
+                    j.set_speed(i_speed)
+                #update des trains
+        for p,train in enumerate(self.trains):
+            if not collision[p]:
+                if train.update():
+                    self.trains.remove(train)
+            col = train.getColors()
+            for k,v in enumerate(train.getPositions()):
+                self.strip.setPixelColor(v,col[k])
+            
+            
     def getAcc(self, position):
         for i in self.boundsFalling:
             if i[0] <= position <= i[1]:
-                return self.g
+                if self.goingUp:
+                    return self.g
+                else:
+                    return - self.g
         for i in self.boundsRising:
             if i[0] <= position <= i[1]:
-                return -self.g
+                if self.goingUp:
+                    return - self.g
+                else:
+                    return self.g
         return 0
     def makeNewTrain(self):
         if self.goingUp:
             position = min(self.bounds)
+            speed = self.speed+random.random()*self.randomSpeed
         else:
             position = max(self.bounds)
+            speed = -self.speed-random.random()*self.randomSpeed
         if self.randomColor:
-            newTrainColor = tuple([random.randint(3,255) for k in range(3)])
+            newTrainColor = self.wheel(random.randint(0,15)*51)
         else:
             newTrainColor = self.color  
     
-        self.trains.append(Train(self, position, self.speed, self.minSpeed, self.getAcc(position) , self.goingUp, self.bounds , self.tailFactor, newTrainColor))
-    
+        self.trains.append(Train(self, position, speed, self.minSpeed, self.getAcc(position) , self.goingUp, self.bounds , self.tailFactor, newTrainColor))
+
+    def wheel(self,value):
+        value = value % (3*255)
+        if value <= 255:
+            r = 255 - value
+            g = value
+            b = 0
+        elif value <= 2*255:
+            r = 0
+            g = 2*255-value
+            b = value-255
+        else:
+            r = value-2*255
+            g = 0
+            b = 3*255-value
+        return (r,g,b)
